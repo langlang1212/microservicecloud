d25860d1cc974ca54817f24203e770b72b3bba1a
langlang1212 <350976906@qq.com>
2019-06-11 21:44:55 +0800

    提交springcloud项目

---
+++ springboot/microservicecloud/microservicecloud-provider-dept-8001/src/test/java/com/test/Test01.java
@@ -0,0 +1,15 @@
+package com.test;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		int currentIndex = 0;
+		
+
+        	System.out.println("currentIndex:"+currentIndex);
+        	currentIndex++;
+        	System.out.println("currentIndex:"+currentIndex);
+
+      }
+
+}
